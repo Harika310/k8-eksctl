6e43a5bddeb693954a03f2f35d6dd1fd0a457556
Harika Eravathri <harikaeravathri@gmail.com>
2024-11-05 11:32:04 -0800

    kubernetes

--- workstation.sh
+++ workstation.sh
@@ -43,6 +43,7 @@ fi
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
+kubectl version
 VALIDATE $? "kubectl installation"
 
 # eksctl
